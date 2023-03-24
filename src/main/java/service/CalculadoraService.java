4be9a3b2e3dbe0c88405a139cb1865f33924214f
jessica <jsouzasouza120@gmail.com>
2023-03-23 21:38:16 -0300

    Inclusão dos métodos para calculos e comparações das áreas de quadrados e triângulos

--- src/main/java/service/CalculadoraService.java
+++ src/main/java/service/CalculadoraService.java
@@ -1,32 +1,62 @@
 package service;
 
+import model.Quadrado;
+import model.Triangulo;
+
 public class CalculadoraService {
 
     public int somar(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     public int subtrair(int a, int b) {
-        return a-b;
+        return a - b;
     }
 
     public int dividir(int a, int b) {
-        return a/b;
+        return a / b;
     }
 
     public int multiplicar(int a, int b) {
-        return a*b;
+        return a * b;
     }
 
 
+    public Double calcularAreaQuadrado(Quadrado quadrado) {
+        if (quadrado != null) {
+            return quadrado.getLado() * quadrado.getLado();
+        } else {
+            System.err.println("Esse quadrado não é válido");
+            return null;
+        }
+    }
 
-//    public ??? calcularArea() {
-//    }
+    public Double calcularAreaTriangulo(Triangulo triangulo) {
+        if (triangulo != null) {
+            return (triangulo.getBase() * triangulo.getAltura()) / 2;
+        } else {
+            System.err.println("Esse triângulo não é válido");
+            return null;
+        }
+    }
 
-    // Caso seja de area igual, retorna null
-//    public Triangulo trianguloDeMenorArea(Triangulo triangulo1, Triangulo triangulo2) {
-//    }
-//    public Quadrado quadradoDeMenorArea(Quadrado quadrado1, Quadrado quadrado2) {
-//    }
+    public Quadrado quadradoDeMenorArea(Quadrado quadradoA, Quadrado quadradoB) {
+        if (calcularAreaQuadrado(quadradoA) < calcularAreaQuadrado(quadradoB)) {
+            return quadradoA;
+        } else if (calcularAreaQuadrado(quadradoB) < calcularAreaQuadrado(quadradoA)) {
+            return quadradoB;
+        } else {
+            return null;
+        }
+    }
 
+    public Triangulo trianguloDeMenorArea(Triangulo trianguloA, Triangulo trianguloB) {
+        if (calcularAreaTriangulo(trianguloB) < calcularAreaTriangulo(trianguloA)) {
+            return trianguloB;
+        } else if (calcularAreaTriangulo(trianguloA) < calcularAreaTriangulo(trianguloB)) {
+            return trianguloA;
+        } else {
+            return null;
+        }
+    }
 }
